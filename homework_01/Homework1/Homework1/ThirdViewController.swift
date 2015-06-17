3535a5b4d2d7ecd0cb80ca04778be387fe08df6c
Colloque Tsui <colloquet@gmail.com>
2015-06-17 19:11:59 +0800

    fix for third controller

--- homework_01/Homework1/Homework1/ThirdViewController.swift
+++ homework_01/Homework1/Homework1/ThirdViewController.swift
@@ -11,17 +11,16 @@ class ThirdViewController: UIViewController {
     @IBOutlet weak var label: UILabel!
     
     @IBAction func calculateEven(sender: AnyObject) {
-        var input:Int! = numberTextField.text.toInt()
+        var input = numberTextField.text.toInt()
         // validation, probably not needed as the keyboard type is numberpad
-        if input == nil {
-            println("Please enter a number!")
-            return
-        }
-        
-        if input % 2 == 0 {
-            label.text = "This is an even number!"
+        if let input = input {
+            if input % 2 == 0 {
+                label.text = "This is an even number!"
+            } else {
+                label.text = "This is not an even number!"
+            }
         } else {
-            label.text = "This is not an even number!"
+            println("Please enter a number!")
         }
     }
 }
